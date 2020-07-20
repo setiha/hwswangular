9dcc1356c95331c65bc70cec2b3277af50ef14e4
Tihici <tihamer.sebok@gmail.com>
2020-07-20 12:28:00 +0300

    lifecycle hook methods :ngOnInit,ngOnChanges, ngAfterViewInit,ngOnDestroy -attribute directives [ngStyle],  -structural directives- *ngIf,  - @ViewChild  -change detection cycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularproject';
+  config = {color: 'red', isUpperCase: true};
+  isComponentVisible = true;
+
+  buttonClicked(clickCounter: number): any {
+    if (clickCounter >= 15) {
+      this.isComponentVisible = false;
+    }
+    else if (clickCounter >= 10) {
+      this.title = 'Nice job';
+    }
+  }
 }
--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output,
+  SimpleChanges, ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.css']
 })
-export class FirstComponent implements OnInit {
+export class FirstComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
+  clickCounter = 0;
+  changeDetectionCounter = 0;
+  @Input() message = 'my first message';
+  @Input() config: {color: string, isUpperCase: boolean};
+  @Output() buttonClick = new EventEmitter<number>();
+  @ViewChild('btn') button: ElementRef <HTMLButtonElement>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('ctr');
   }
 
+  ngOnInit(): void {
+    console.log('on init');
+  }
+  ngOnChanges(changes: SimpleChanges): void{
+    console.log('on changes', changes);
+  }
+  ngAfterViewInit(): void {
+    console.log('after view init');
+  }
+  increaseCounter(): any {
+    this.clickCounter++;
+    this.buttonClick.emit(this.clickCounter);
+    this.button.nativeElement.textContent = 'dont click me';
+  }
+  ngOnDestroy(): void {
+    console.log('on destroy');
+  }
+  get log(): any {
+    this.changeDetectionCounter++
+    console.log(this.changeDetectionCounter);
+    return 'test';
+  }
 }
