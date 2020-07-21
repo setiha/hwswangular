395c86f89f4641d12bbe56a1617347ae25060a92
Tihici <tihamer.sebok@gmail.com>
2020-07-21 11:06:10 +0300

    a 2 ik eloadas vege

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {green} from "color-name";
+import {isUpperCase} from "tslint/lib/utils";
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,14 @@ export class AppComponent {
   title = 'angularproject';
   config = {color: 'red', isUpperCase: true};
   isComponentVisible = true;
-
   buttonClicked(clickCounter: number): any {
     if (clickCounter >= 15) {
       this.isComponentVisible = false;
     }
     else if (clickCounter >= 10) {
       this.title = 'Nice job';
+      this.config.color = 'green';
+      this.config.isUpperCase = false;
     }
   }
 }
